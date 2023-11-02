75bfb59856d0308b563a10771fda471913fad2dc
cmark0v <ubuntu@ubuntu.com>
2023-11-02 12:53:32 +0900

    verticle align and optional bloc quote

--- lib/natspecgen.py
+++ lib/natspecgen.py
@@ -1,35 +1,41 @@
 #!/usr/bin/env python3
 import __main__
 import sys
+import os
 import json
 import argparse
 import subprocess
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BQUOTE = os.getenv("BQUOTE", True)
 
 ap = argparse.ArgumentParser("natgenspec.py")
 ap.add_argument("file", help="File to parse with solc.")
 ap.add_argument("line", help="Line number of definition", type=int)
-ap.add_argument("-i", "--indent", default=0, help="Indentation level in space count.", type=int)
+ap.add_argument(
+    "-i", "--indent", default=0, help="Indentation level in space count.", type=int
+)
 
 args = ap.parse_args()
 
-solc_out = subprocess.check_output(['solc', '--ast-compact-json', args.file]).decode('utf8')
-solc_split = solc_out.split('\n')
+solc_out = subprocess.check_output(["solc", "--ast-compact-json", args.file]).decode(
+    "utf8"
+)
+solc_split = solc_out.split("\n")
 
-author_name = subprocess.check_output([
-    'git',
-    'config',
-    'user.name'
-]).decode('utf8').strip()
-author_email = subprocess.check_output([
-    'git',
-    'config',
-    'user.email'
-]).decode('utf8').strip()
+author_name = (
+    subprocess.check_output(["git", "config", "user.name"]).decode("utf8").strip()
+)
+author_email = (
+    subprocess.check_output(["git", "config", "user.email"]).decode("utf8").strip()
+)
 
 linebreak_byte_numbers = []
-with open(args.file, 'r') as f:
+with open(args.file, "r") as f:
     for i, b in enumerate(f.read()):
-        if b == '\n':
+        if b == "\n":
             linebreak_byte_numbers.append(i)
 
 start = None
@@ -37,27 +43,36 @@ end = None
 
 for i, line in enumerate(solc_split):
     if start is not None:
-        if '====' in line:
+        if "====" in line:
             end = i
             break
-    if '====' in line and args.file in line:
+    if "====" in line and args.file in line:
         start = i
 
-ast_json = json.loads(
-    "\n".join(solc_split[start+1:end])
-)
+ast_json = json.loads("\n".join(solc_split[start + 1 : end]))
 params = []
 returnz = []
+
+
 def line_dfs(node):
     # solc AST doesnt give us line numbers LMAO
-    byte_pos = int(node.get('src').split(':')[0])
+    byte_pos = int(node.get("src").split(":")[0])
     # hacker moment
-    line_nr = min(line for (line, byte) in enumerate(linebreak_byte_numbers) if byte >= byte_pos)
-    if line_nr == args.line and node.get('nodeType') in ['ContractDefinition', 'FunctionDefinition']:
-        __main__.params = __main__.params + getnames(node.get('parameters',{}).get('parameters'))
-        __main__.returnz = __main__.returnz +  getnames(node.get('returnParameters',{}).get('parameters'))
+    line_nr = min(
+        line for (line, byte) in enumerate(linebreak_byte_numbers) if byte >= byte_pos
+    )
+    if line_nr == args.line and node.get("nodeType") in [
+        "ContractDefinition",
+        "FunctionDefinition",
+    ]:
+        __main__.params = __main__.params + getnames(
+            node.get("parameters", {}).get("parameters")
+        )
+        __main__.returnz = __main__.returnz + getnames(
+            node.get("returnParameters", {}).get("parameters")
+        )
     else:
-        nodes = node.get('nodes')
+        nodes = node.get("nodes")
         if not nodes:
             return None
         else:
@@ -66,6 +81,7 @@ def line_dfs(node):
                 if success:
                     return success
 
+
 def getnames(bob):
     out = []
     if type(bob) is list:
@@ -73,14 +89,15 @@ def getnames(bob):
             out.append(b)
     return out
 
+
 def print_indented(string):
-    print(' '*int(args.indent) + string)
+    print(" " * int(args.indent) + string)
+
 
 if args.line:
     symbol_node = line_dfs(ast_json)
-#else:
+# else:
 #    symbol_node = dfs(ast_json)
-
 
 
 #    print_indented(
@@ -89,10 +106,25 @@ if args.line:
 #    print_indented(
 #        f"/// @title {symbol_node['attributes']['nodeType']}"
 #    )
-print_indented(f"/// @notice")
-print_indented(f"/// @dev")
+if BQUOTE:
+    bhead = "/**"
+    btail = " */"
+    lhead = "    "
+else:
+    bhead = "///"
+    btail = ""
+    lhead = "///"
+maxl = 0
 for param in params:
-    print_indented(f"/// @param {param['name']} {param['typeName']['name']}")
-for param in returnz:
-    print_indented(f"/// @return {param['name']} {param['typeName']['name']}")
+    maxl = max(maxl, len(param["name"]))
 
+
+print_indented(f"{bhead} @notice")
+print_indented(f"{lhead} @dev")
+for param in params:
+    paramN = param["name"] + " " * (maxl - len(param["name"]))
+    print_indented(f"{lhead} @param  {paramN} {param['typeName']['name']}")
+for param in returnz:
+    print_indented(f"{lhead} @return {param['name']} {param['typeName']['name']}")
+
+print_indented(f"{btail}")
